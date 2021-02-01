1cfc118b3c4404680ad11b79946fb526793cecc5
sseyeon <seyeon9809@gmail.com>
2021-02-01 20:22:47 +0900

    README.md 파일 커밋, dao까지

---
+++ src/main/java/com/sseyeon/board/dao/BoardDAO.java
@@ -0,0 +1,14 @@
+package com.sseyeon.board.dao;
+
+import java.util.List;
+
+import com.sseyeon.board.model.BoardVO;
+
+public interface BoardDAO {
+	public List<BoardVO> getBoardList() throws Exception;
+	public BoardVO getBoardContent(int bid) throws Exception;
+	public int insertBoard(BoardVO boardVO) throws Exception;
+	public int updateBoard(BoardVO boardVO) throws Exception;
+	public int deleteBoard(int bid) throws Exception;
+	public int updateViewCnt(int bid) throws Exception;
+}
---
+++ src/main/java/com/sseyeon/board/dao/BoardDAOImpl.java
@@ -0,0 +1,49 @@
+package com.sseyeon.board.dao;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Repository;
+
+import com.sseyeon.board.model.BoardVO;
+
+@Repository
+public class BoardDAOImpl implements BoardDAO{
+	
+	@Inject
+	private SqlSession sqlSession;
+
+	@Override
+	public List<BoardVO> getBoardList() throws Exception {
+		return sqlSession.selectList("com.sseyeon.board.boardMapper.getBoardList");
+	}
+
+	@Override
+	public BoardVO getBoardContent(int bid) throws Exception {
+		return sqlSession.selectOne("com.sseyeon.board.boardMapper.getBoardContent", bid);
+	}
+
+	@Override
+	public int insertBoard(BoardVO boardVO) throws Exception {
+		return sqlSession.insert("com.sseyeon.board.boardMapper.insertBoard", boardVO);
+	}
+
+	@Override
+	public int updateBoard(BoardVO boardVO) throws Exception {
+		return sqlSession.update("com.sseyeon.board.boardBapper.updateBoard", boardVO);
+	}
+
+	@Override
+	public int deleteBoard(int bid) throws Exception {
+		return sqlSession.delete("com.sseyeon.board.boardMapper.deleteBorad", bid);
+	}
+
+	@Override
+	public int updateViewCnt(int bid) throws Exception {
+		return sqlSession.update("com.sseyeon.board.boardMapper.updateViewCnt", bid);
+	}
+	
+	
+}
